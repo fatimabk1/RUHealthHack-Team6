5f30c8b4f0202deed8a02d2cbd9ccfa89c5c670c
fatimabk1 <fatima.kahbi@gmail.com>
2023-11-04 13:12:10 -0400

    Initial Commit

---
+++ RUHealthHack-Team6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RUHealthHack-Team6
+//
+//  Created by Fatima Kahbi on 11/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RUHealthHack-Team6/RUHealthHack_Team6App.swift
@@ -0,0 +1,17 @@
+//
+//  RUHealthHack_Team6App.swift
+//  RUHealthHack-Team6
+//
+//  Created by Fatima Kahbi on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct RUHealthHack_Team6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
